26f56361d6586f2704a938b5e1666ad6289bbef5
Anar <153716590+Anar0204@users.noreply.github.com>
2025-05-22 00:00:12 +0400

    Add files via upload

--- ConsoleApp1/TaskManagmentApp/Program.cs
+++ ConsoleApp1/TaskManagmentApp/Program.cs
@@ -12,5 +12,19 @@ Task1 task3 = new Task1("Yemek bisir", TaskStatus1.InProgress);
 
 Console.WriteLine(task1.ToString());
 
+string[] strings = new string[4];
+
+
+
+SimpleStringCollection simpleStringCollection = new SimpleStringCollection(strings);
+
+simpleStringCollection[1] = "Kitap";
+
+string name = simpleStringCollection[1];
+
+Console.WriteLine( name);
+
+
+
 
 
---
+++ ConsoleApp1/TaskManagmentApp/SimpleStringCollection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagmentApp
+{
+    internal class SimpleStringCollection
+    {
+        string[] Strings;
+
+        public SimpleStringCollection(string[] strings)
+        {
+            Strings = strings;
+        }
+
+        public string this[int index]
+        {
+            get
+            {
+                return Strings[index];
+            }
+            set
+            {
+                Strings[index] = value;
+            }
+        }
+    }
+}
